7e7a94b09b5d70d766c32e453b88b3454507b9d0
RuiYang Ju <102137894+RuiyangJu@users.noreply.github.com>
2023-05-24 17:24:54 +0800

    Add files via upload

---
+++ imgaug.py
@@ -0,0 +1,19 @@
+import os
+import cv2
+import numpy as np
+
+imgdir = "../data/images/"
+# labeldir = "../labels/"
+
+for root, dirs, files in os.walk(imgdir):
+    for name in files:
+        img_name = os.path.join(name)
+        print(img_name)
+        pic = cv2.imread(imgdir + img_name)
+        contrast = 1.2
+        brightness = 30
+        pic_turn = cv2.addWeighted(pic, contrast, pic, 0, brightness)
+        cv2.imwrite(imgdir + 'aug_' + img_name, pic_turn)
+        # cv2.imshow('original', pic)
+        # cv2.imshow('turn', pic_turn)
+        cv2.waitKey(0)
